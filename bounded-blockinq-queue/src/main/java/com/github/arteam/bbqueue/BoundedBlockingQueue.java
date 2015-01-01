7b343cbd41ca24b3d7810e3f4f78630ddefd2618
Artem Prigoda <aprigoda@bcc.ru>
2015-01-01 22:04:48 +0300

    Bounded blocking queue

---
+++ bounded-blockinq-queue/src/main/java/com/github/arteam/bbqueue/BoundedBlockingQueue.java
@@ -0,0 +1,233 @@
+package com.github.arteam.bbqueue;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Date: 1/1/15
+ * Time: 5:45 PM
+ *
+ * @author Artem Prigoda
+ */
+public class BoundedBlockingQueue<T> implements BlockingQueue<T> {
+
+    private final ArrayDeque<T> deque;
+    private final int limit;
+
+    public BoundedBlockingQueue(int limit) {
+        this.limit = limit;
+        deque = new ArrayDeque<T>(limit);
+    }
+
+    @Override
+    public synchronized boolean add(T t) {
+        if (!offer(t)) {
+            throw new IllegalStateException("Queue is full");
+        }
+        return true;
+    }
+
+    @Override
+    public synchronized boolean offer(T t) {
+        return deque.size() < limit && addAndNotify(t);
+    }
+
+    @Override
+    public synchronized void put(T t) throws InterruptedException {
+        while (deque.size() >= limit) {
+            wait();
+        }
+        addAndNotify(t);
+    }
+
+    @Override
+    public boolean offer(T t, long timeout, TimeUnit unit) throws InterruptedException {
+        int enters = 0;
+        synchronized (this) {
+            while (deque.size() >= limit) {
+                if (enters++ > 0) {
+                    return false;
+                }
+                wait(unit.toMillis(timeout));
+            }
+            return addAndNotify(t);
+        }
+    }
+
+    private boolean addAndNotify(T t) {
+        deque.add(t);
+        notifyAll();
+        return true;
+    }
+
+    @Override
+    public synchronized T remove() {
+        T head = poll();
+        if (head == null) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return head;
+    }
+
+    @Override
+    public synchronized T poll() {
+        if (deque.isEmpty()) {
+            return null;
+        }
+        return removeAndNotify();
+    }
+
+    @Override
+    public synchronized T take() throws InterruptedException {
+        while (deque.isEmpty()) {
+            wait();
+        }
+        return removeAndNotify();
+    }
+
+    @Override
+    public synchronized T poll(long timeout, TimeUnit unit) throws InterruptedException {
+        int enters = 0;
+        synchronized (this) {
+            while (deque.isEmpty()) {
+                if (enters++ > 0) {
+                    return null;
+                }
+                wait(unit.toMillis(timeout));
+            }
+            return removeAndNotify();
+        }
+    }
+
+    private T removeAndNotify() {
+        T head = deque.remove();
+        notifyAll();
+        return head;
+    }
+
+    @Override
+    public synchronized boolean remove(Object o) {
+        boolean removed = deque.remove(o);
+        if (removed) {
+            notifyAll();
+        }
+        return removed;
+    }
+
+    @Override
+    public synchronized int drainTo(Collection<? super T> c) {
+        if (c == this) {
+            throw new IllegalArgumentException("Unable drain to itself");
+        }
+        int transferred = deque.size();
+        c.addAll(deque);
+        deque.clear();
+
+        notifyAll();
+        return transferred;
+    }
+
+    @Override
+    public synchronized int drainTo(Collection<? super T> c, int maxElements) {
+        if (c == this) {
+            throw new IllegalArgumentException("Unable drain to itself");
+        }
+        if (maxElements <= 0) {
+            return 0;
+        }
+        int transferred = Math.min(maxElements, deque.size());
+        for (int i = 0; i < transferred; i++) {
+            c.add(deque.remove());
+        }
+
+        notifyAll();
+        return transferred;
+    }
+
+    @Override
+    public synchronized int remainingCapacity() {
+        return limit - deque.size();
+    }
+
+    @Override
+    public synchronized boolean contains(Object o) {
+        return deque.contains(o);
+    }
+
+    @Override
+    public synchronized T element() {
+        return deque.element();
+    }
+
+    @Override
+    public synchronized T peek() {
+        return deque.peek();
+    }
+
+    @Override
+    public synchronized int size() {
+        return deque.size();
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        return deque.isEmpty();
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return new ArrayList<T>(deque).iterator();
+    }
+
+    @Override
+    public synchronized Object[] toArray() {
+        return deque.toArray();
+    }
+
+    @Override
+    public synchronized <T> T[] toArray(T[] a) {
+        return deque.toArray(a);
+    }
+
+    @Override
+    public synchronized boolean containsAll(Collection<?> c) {
+        return deque.containsAll(c);
+    }
+
+    @Override
+    public synchronized boolean addAll(Collection<? extends T> c) {
+        boolean added = deque.addAll(c);
+        if (added) {
+            notifyAll();
+        }
+        return added;
+    }
+
+    @Override
+    public synchronized boolean removeAll(Collection<?> c) {
+        boolean removed = deque.removeAll(c);
+        if (removed) {
+            notifyAll();
+        }
+        return removed;
+    }
+
+    @Override
+    public synchronized boolean retainAll(Collection<?> c) {
+        boolean retained = deque.retainAll(c);
+        if (retained) {
+            notifyAll();
+        }
+        return retained;
+    }
+
+    @Override
+    public synchronized void clear() {
+        deque.clear();
+        notifyAll();
+    }
+}
---
+++ bounded-blockinq-queue/src/test/java/BoundedBlockingQueueSize.java
@@ -0,0 +1,248 @@
+import com.github.arteam.bbqueue.BoundedBlockingQueue;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Date: 1/1/15
+ * Time: 8:33 PM
+ * Basic sanity tests
+ *
+ * @author Artem Prigoda
+ */
+public class BoundedBlockingQueueSize {
+
+    final BlockingQueue<Long> queue = new BoundedBlockingQueue<Long>(10);
+    final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    private void initQueue() {
+        for (int i = 0; i < 10; i++) {
+            queue.add(random.nextLong());
+        }
+    }
+
+    @Test
+    public void test10Adds() throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (int i = 0; i < 10; i++) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    assertTrue(queue.add(random.nextLong()));
+                }
+            });
+        }
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+        assertEquals(queue.size(), 10);
+    }
+
+    @Test
+    public void test11Adds() throws InterruptedException {
+        final AtomicInteger successes = new AtomicInteger(0);
+        final AtomicInteger fails = new AtomicInteger(0);
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (int i = 0; i < 11; i++) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        queue.add(random.nextLong());
+                        successes.incrementAndGet();
+                    } catch (IllegalStateException e) {
+                        fails.incrementAndGet();
+                    }
+                }
+            });
+        }
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+
+        assertEquals(queue.size(), 10);
+        assertEquals(successes.get(), 10);
+        assertEquals(fails.get(), 1);
+    }
+
+    @Test
+    public void test11Offers() throws InterruptedException {
+        final AtomicInteger successes = new AtomicInteger(0);
+        final AtomicInteger fails = new AtomicInteger(0);
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (int i = 0; i < 11; i++) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    boolean offered = queue.offer(random.nextLong());
+                    if (offered) {
+                        successes.incrementAndGet();
+                    } else {
+                        fails.incrementAndGet();
+                    }
+                }
+            });
+        }
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+        assertEquals(queue.size(), 10);
+        assertEquals(successes.get(), 10);
+        assertEquals(fails.get(), 1);
+    }
+
+
+    @Test
+    public void testPut() throws InterruptedException {
+        initQueue();
+
+        ExecutorService putExecutor = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+            putExecutor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        queue.put(random.nextLong());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        ExecutorService removeExecutor = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+            removeExecutor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    queue.remove();
+                }
+            });
+        }
+
+        putExecutor.shutdown();
+        removeExecutor.shutdown();
+
+        putExecutor.awaitTermination(5, TimeUnit.MINUTES);
+        removeExecutor.awaitTermination(5, TimeUnit.MINUTES);
+
+        assertEquals(queue.size(), 10);
+    }
+
+    @Test
+    public void testOfferWithTimeout() throws InterruptedException {
+        initQueue();
+
+        boolean offered = queue.offer(random.nextLong(), 10, TimeUnit.MILLISECONDS);
+        Assert.assertFalse(offered);
+    }
+
+
+    @Test
+    public void test11Removes() throws InterruptedException {
+        initQueue();
+
+        final AtomicInteger successes = new AtomicInteger(0);
+        final AtomicInteger fails = new AtomicInteger(0);
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (int i = 0; i < 11; i++) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        queue.remove();
+                        successes.incrementAndGet();
+                    } catch (IllegalStateException e) {
+                        fails.incrementAndGet();
+                    }
+                }
+            });
+        }
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+        assertEquals(queue.size(), 0);
+        assertEquals(successes.get(), 10);
+        assertEquals(fails.get(), 1);
+    }
+
+    @Test
+    public void testPoll() throws InterruptedException {
+        initQueue();
+
+        final AtomicInteger successes = new AtomicInteger(0);
+        final AtomicInteger fails = new AtomicInteger(0);
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (int i = 0; i < 11; i++) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    Long result = queue.poll();
+                    if (result != null) {
+                        successes.incrementAndGet();
+                    } else {
+                        fails.incrementAndGet();
+                    }
+                }
+            });
+        }
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+        assertEquals(queue.size(), 0);
+        assertEquals(successes.get(), 10);
+        assertEquals(fails.get(), 1);
+    }
+
+    @Test
+    public void testPollWithTimeout() throws Exception {
+        assertNull(queue.poll(10, TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void testTake() throws InterruptedException {
+        ExecutorService takeExecutor = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+            takeExecutor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        queue.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        ExecutorService putExecutor = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+            putExecutor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        queue.put(random.nextLong());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        takeExecutor.shutdown();
+        putExecutor.shutdown();
+
+        takeExecutor.awaitTermination(5, TimeUnit.MINUTES);
+        putExecutor.awaitTermination(5, TimeUnit.MINUTES);
+
+        assertEquals(queue.size(), 0);
+    }
+
+}
